1bc548d29db7ace96b69049c8abc62e491e1f266
Martin <martin.dinahet@proton.me>
2025-09-24 19:15:30 +0200

    feature(auth): added sign-in page

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,4 +1,4 @@
-import { authServer } from "@/lib/auth/server";
+import { authServer } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
 export const { POST, GET } = toNextJsHandler(authServer);
--- src/lib/actions/sign-in.ts
+++ src/lib/actions/sign-in.ts
@@ -1,10 +1,14 @@
 "use server";
 
-import { authServer } from "@/lib/auth/server";
+import { authServer } from "@/lib/auth";
 
 export const signIn = async (_prevState: unknown, formData: FormData) => {
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
-  await authServer.api.signInEmail({ body: { email, password } });
-  return { success: true };
+  try {
+    await authServer.api.signInEmail({ body: { email, password } });
+    return { success: true };
+  } catch (error) {
+    return { success: false, error: "Unable to login" };
+  }
 };
