37d1bad920af274378a1894716c07e05de9ad0d3
Martin <martin.dinahet@proton.me>
2025-09-24 17:48:31 +0200

    feature(auth): added betterauth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
 import type { FC } from "react";
 
-const Page: FC = () => {
+const Page: FC = async () => {
+  const users = await prisma.user.findMany();
+  users.forEach((user) => console.log(user));
   return <div>Hello, World!</div>;
 };
 
