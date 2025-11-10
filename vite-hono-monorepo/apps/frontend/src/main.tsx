31634b9eb13cd244e0d7d73ee1edcd1959da7e45
Martin <martin.dinahet@proton.me>
2025-11-11 00:10:35 +0100

    feature(dep): added biome linter & formatter, formatted all project

--- vite-hono-monorepo/apps/frontend/src/main.tsx
+++ vite-hono-monorepo/apps/frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { IndexPage } from "./pages/index.page";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// biome-ignore lint/style/noNonNullAssertion: what else am i even suppposed to do here
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<IndexPage />
+	</StrictMode>,
+);
---
+++ vite-hono-monorepo/apps/frontend/src/pages/index.page.tsx
@@ -0,0 +1,7 @@
+export function IndexPage() {
+	return (
+		<div>
+			<h1>Hello, World!</h1>
+		</div>
+	);
+}
