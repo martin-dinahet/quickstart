b7498e54d85339e4855e7462939958b0491696e0
Martin <martin.dinahet@proton.me>
2025-11-11 01:11:17 +0100

    refactor(paths,frontend): removed path aliases for concistency

--- vite-hono-monorepo/apps/frontend/src/main.tsx
+++ vite-hono-monorepo/apps/frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
-import { DefaultLayout } from "@/layouts/default.layout";
-import { IndexPage } from "@/pages/index.page";
+import { DefaultLayout } from "./layouts/default.layout";
+import { IndexPage } from "./pages/index.page";
 
 // biome-ignore lint/style/noNonNullAssertion: what else am i even suppposed to do here
 createRoot(document.getElementById("root")!).render(
