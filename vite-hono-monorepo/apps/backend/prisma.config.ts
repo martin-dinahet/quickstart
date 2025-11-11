56b90de1d2f9f43fe571150588e92a4796a52aaf
Martin <martin.dinahet@proton.me>
2025-11-11 01:09:26 +0100

    feature(dep,orm,db): added prisma to backend, added docker setup

---
+++ vite-hono-monorepo/apps/backend/prisma.config.ts
@@ -0,0 +1,16 @@
+import { resolve } from "node:path";
+import { config } from "dotenv";
+import { defineConfig, env } from "prisma/config";
+
+config({ path: resolve(__dirname, ".env") });
+
+export default defineConfig({
+	schema: resolve(__dirname, "prisma/schema.prisma"),
+	migrations: {
+		path: "prisma/migrations",
+	},
+	engine: "classic",
+	datasource: {
+		url: env("DATABASE_URL"),
+	},
+});
